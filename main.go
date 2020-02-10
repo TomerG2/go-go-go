17f27a0fdfe836bfcc2fbb3c700baa0ef095850b
Tomer Golan <tomer.g2@gmail.com>
2020-02-10 07:57:32 +0200

    wip

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"sync"
 	"time"
 )
 
@@ -16,44 +15,19 @@ func Say(s string) {
 }
 
 func findUserById(uid string) string {
-	defer wg.Done()
 	time.Sleep(time.Second)
 	fmt.Println("user found")
 	return "John"
 }
 
 func checkSubStatus(uid string) bool {
-	defer wg.Done()
 	time.Sleep(time.Second)
 	fmt.Println("subscription found")
 	return true
 }
 
 func getQuotes(uid string) int {
-	defer wg.Done()
 	time.Sleep(time.Second)
 	fmt.Println("quote found")
 	return 3000
-}
-
-func elapsed(what string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Printf("%s took %v\n", what, time.Since(start))
-	}
-}
-
-var wg sync.WaitGroup
-
-func main() {
-	defer elapsed("quote API")()
-	wg.Add(1)
-	go findUserById("a1")
-	wg.Add(1)
-	go checkSubStatus("a1")
-	wg.Wait()
-
-	wg.Add(1)
-	go getQuotes("a1")
-	wg.Wait()
 }
--- main_test.go
+++ main_test.go
@@ -8,3 +8,9 @@ func TestExample_1(t *testing.T) {
 	Say("hey")
 	Say("there")
 }
+
+func TestExample_2(t *testing.T) {
+	findUserById("a1")
+	checkSubStatus("a1")
+	getQuotes("a1")
+}
