dbe9eb9af56ebea3f119e719fc0b43a9c1d9773f
Tomer Golan <tomer.g2@gmail.com>
2020-02-11 00:54:30 +0200

    moved helper func

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Helper functions
+func getUser(s string) {
+	time.Sleep(funcWaitTime)
+	fmt.Println("user found !")
+}
+
+func getUserSub(s string) {
+	time.Sleep(funcWaitTime)
+	fmt.Println("subscription found !")
+}
+
+func generateQuote(s string) {
+	time.Sleep(funcWaitTime)
+	fmt.Println("quote generated !")
+}
+
+func getUserWait(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(funcWaitTime)
+	fmt.Println("user found !")
+}
+
+func getUserSubWait(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(funcWaitTime)
+	fmt.Println("subscription found !")
+}
+
+func generateQuoteWait(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(funcWaitTime)
+	fmt.Println("quote generated !")
+}
+
+var funcWaitTime = time.Millisecond * 300
--- main_test.go
+++ main_test.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"testing"
-	"time"
 )
 
-func TestQuoteAPI_Procedural(t *testing.T) {
+func TestQuoteAPI_Sync(t *testing.T) {
 	getUser("a1")
 	getUserSub("a1")
 	generateQuote("a1")
@@ -30,39 +28,3 @@ func TestQuoteAPI_WaitGroup(t *testing.T) {
 
 	wg.Wait()
 }
-
-// Helper functions
-func getUser(s string) {
-	time.Sleep(funcWaitTime)
-	fmt.Println("user found !")
-}
-
-func getUserSub(s string) {
-	time.Sleep(funcWaitTime)
-	fmt.Println("subscription found !")
-}
-
-func generateQuote(s string) {
-	time.Sleep(funcWaitTime)
-	fmt.Println("quote generated !")
-}
-
-func getUserWait(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(funcWaitTime)
-	fmt.Println("user found !")
-}
-
-func getUserSubWait(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(funcWaitTime)
-	fmt.Println("subscription found !")
-}
-
-func generateQuoteWait(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(funcWaitTime)
-	fmt.Println("quote generated !")
-}
-
-var funcWaitTime = time.Millisecond * 300
