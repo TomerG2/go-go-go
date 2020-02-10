627a64535f30043c18ffa4df606352f6cc2d6b68
Tomer Golan <tomer.g2@gmail.com>
2020-02-11 00:35:20 +0200

    wait group example

--- main.go
+++ main.go
@@ -1,15 +1,1 @@
 package main
-
-import "time"
-
-func generateQuote(s string) {
-	time.Sleep(time.Millisecond * 500)
-}
-
-func getUserSub(s string) {
-	time.Sleep(time.Millisecond * 500)
-}
-
-func getUser(s string) {
-	time.Sleep(time.Millisecond * 500)
-}
--- main_test.go
+++ main_test.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestExample_Intro(t *testing.T) {
+func TestExample_LinearSolution(t *testing.T) {
 	getUser("a1")
 	getUserSub("a1")
 	generateQuote("a1")
+}
+
+var wg sync.WaitGroup
+
+func TestExample_WaitGroup(t *testing.T) {
+	wg.Add(1)
+	go getUser("a1")
+	wg.Add(1)
+	go getUserSub("a1")
+	wg.Add(1)
+	go generateQuote("a1")
+
+	wg.Wait()
+}
+
+func getUser(s string) {
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("user found !")
+	wg.Done()
+}
+
+func getUserSub(s string) {
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("subscription found !")
+	wg.Done()
+}
+
+func generateQuote(s string) {
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("quote generated !")
+	wg.Done()
 }
