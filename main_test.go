307e1a3fab9e3a2fcdb0fa1705d40c86dc439e89
Tomer Golan <tomer.g2@gmail.com>
2020-02-10 09:28:12 +0200

    wip

--- main_test.go
+++ main_test.go
@@ -14,3 +14,9 @@ func TestRecycling_LinearSolution(t *testing.T) {
 	plasticOnly := FilterNonPlastic(garbage)
 	RecyclePlastic(plasticOnly)
 }
+
+func TestRecycling_ConcurrentSolution1(t *testing.T) {
+	garbage := []string{"plastic bottle", "plastic bottle", "apple", "banana"}
+	plasticOnly := FilterNonPlastic(garbage)
+	go RecyclePlastic(plasticOnly)
+}
