92dde638b0c15179f1722bd32fa1dc5463fca01d
Tomer Golan <tomer.g2@gmail.com>
2020-02-11 00:24:38 +0200

    basic

--- main.go
+++ main.go
@@ -1,41 +1,15 @@
 package main
 
-import (
-	"strings"
-	"time"
-)
+import "time"
 
-import (
-	"fmt"
-)
-
-func Say(s string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(s)
-		time.Sleep(time.Millisecond * 100)
-	}
+func generateQuote(s string) {
+	time.Sleep(time.Millisecond * 500)
 }
 
-func FilterNonPlastic(garbage []string) []string {
-	fmt.Printf("Garbage: %s \n", strings.Join(garbage, ", "))
-	var plasticOnly []string
-	for _, g := range garbage {
-		time.Sleep(time.Millisecond * 100)
-		if !strings.Contains(g, "plastic") {
-			continue
-		}
-		plasticOnly = append(plasticOnly, g)
-	}
-	fmt.Printf("FilterNonPlastic: %s \n", strings.Join(plasticOnly, ", "))
-	return plasticOnly
+func getUserSub(s string) {
+	time.Sleep(time.Millisecond * 500)
 }
 
-func RecyclePlastic(plastics []string) []string {
-	var rawPlastic []string
-	for _, _ = range plastics {
-		time.Sleep(time.Millisecond * 100)
-		rawPlastic = append(rawPlastic, "raw plastic")
-	}
-	fmt.Printf("FilterNonPlastic: %s \n", strings.Join(rawPlastic, ", "))
-	return plastics
+func getUser(s string) {
+	time.Sleep(time.Millisecond * 500)
 }
--- main_test.go
+++ main_test.go
@@ -2,23 +2,10 @@ package main
 
 import (
 	"testing"
-	"time"
 )
 
 func TestExample_Intro(t *testing.T) {
-	go Say("hey")
-	go Say("there")
-	time.Sleep(time.Second)
-}
-
-func TestRecycling_LinearSolution(t *testing.T) {
-	garbage := []string{"plastic bottle", "plastic bottle", "apple", "banana"}
-	plasticOnly := FilterNonPlastic(garbage)
-	RecyclePlastic(plasticOnly)
-}
-
-func TestRecycling_ConcurrentSolution1(t *testing.T) {
-	garbage := []string{"plastic bottle", "plastic bottle", "apple", "banana"}
-	plasticOnly := FilterNonPlastic(garbage)
-	go RecyclePlastic(plasticOnly)
+	getUser("a1")
+	getUserSub("a1")
+	generateQuote("a1")
 }
