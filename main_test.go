3700b5559dd26a7cbd0f322cc119596ecc069be0
Tomer Golan <tomer.g2@gmail.com>
2020-02-11 00:52:55 +0200

    go routine and wait group

--- main_test.go
+++ main_test.go
@@ -14,54 +14,55 @@ func TestQuoteAPI_Procedural(t *testing.T) {
 }
 
 func TestQuoteAPI_Goroutines(t *testing.T) {
-	getUser("a1")
+	go getUser("a1")
 	getUserSub("a1")
 	generateQuote("a1")
 }
 
-var wg sync.WaitGroup
-
 func TestQuoteAPI_WaitGroup(t *testing.T) {
+	var wg sync.WaitGroup
 	wg.Add(1)
-	go getUserWait("a1")
+	go getUserWait("a1", &wg)
 	wg.Add(1)
-	go getUserSubWait("a1")
+	go getUserSubWait("a1", &wg)
 	wg.Add(1)
-	go generateQuoteWait("a1")
+	go generateQuoteWait("a1", &wg)
 
 	wg.Wait()
 }
 
 // Helper functions
 func getUser(s string) {
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("user found !")
 }
 
 func getUserSub(s string) {
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("subscription found !")
 }
 
 func generateQuote(s string) {
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("quote generated !")
 }
 
-func getUserWait(s string) {
+func getUserWait(s string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("user found !")
 }
 
-func getUserSubWait(s string) {
+func getUserSubWait(s string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("subscription found !")
 }
 
-func generateQuoteWait(s string) {
+func generateQuoteWait(s string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(funcWaitTime)
 	fmt.Println("quote generated !")
 }
+
+var funcWaitTime = time.Millisecond * 300
