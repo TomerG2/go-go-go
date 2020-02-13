84be933307018cd89758132557d4553c0226d9ab
Tomer Golan <tomer.g2@gmail.com>
2020-02-13 14:20:06 +0200

    remove miners

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -44,35 +43,4 @@ func TestQuoteAPI_Channels(t *testing.T) {
 	}()
 	<-doneChan                    // block until go routine signals work is done
 	<-time.After(time.Second * 5) //Receiving from channel after 5 sec
-}
-
-func TestDoneMiners(t *testing.T) {
-	theMine := [5]string{"rock", "ore", "ore", "rock", "ore"}
-	oreChannel := make(chan string)
-	minedOreChan := make(chan string)
-	// Finder
-	go func(mine [5]string) {
-		for _, item := range mine {
-			if item == "ore" {
-				oreChannel <- item //send item on oreChannel
-			}
-		}
-	}(theMine)
-	// Ore Breaker
-	go func() {
-		for i := 0; i < 3; i++ {
-			foundOre := <-oreChannel //read from oreChannel
-			fmt.Println("From Finder: ", foundOre)
-			minedOreChan <- "minedOre" //send to minedOreChan
-		}
-	}()
-	// Smelter
-	go func() {
-		for i := 0; i < 3; i++ {
-			minedOre := <-minedOreChan //read from minedOreChan
-			fmt.Println("From Miner: ", minedOre)
-			fmt.Println("From Smelter: Ore is smelted")
-		}
-	}()
-	<-time.After(time.Second * 5) // Again, you can ignore this
 }
