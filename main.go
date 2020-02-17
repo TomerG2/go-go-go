9c7947ea6705dde133d6f1084dffc0928571121d
Tomer Golan <tomer.g2@gmail.com>
2020-02-17 02:47:46 +0200

    add wait group handler

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var funcWaitTime = time.Millisecond * 300
 func main() {
 	http.HandleFunc("/quote", quoteHandler)
 	http.HandleFunc("/quote/v2", quoteHandlerConcurrent)
+	http.HandleFunc("/quote/v3", quoteHandlerWait)
 	http.ListenAndServe(":8090", nil)
 }
 
@@ -26,6 +27,18 @@ func quoteHandlerConcurrent(w http.ResponseWriter, req *http.Request) {
 	go getUser("a1")
 	go getUserSub("a1")
 	go generateQuote("a1")
+	fmt.Fprintf(w, "Done quote ready\n")
+}
+
+func quoteHandlerWait(w http.ResponseWriter, req *http.Request) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go getUserWait("a1", &wg)
+	wg.Add(1)
+	go getUserSubWait("a1", &wg)
+	wg.Add(1)
+	go generateQuoteWait("a1", &wg)
+	wg.Wait()
 	fmt.Fprintf(w, "Done quote ready\n")
 }
 
